01b853a2eb98c7cd74471d64cb085c2646af8155
rivaldo_d_b_23 <21956936@sun.ac.za>
2020-11-11 21:43:25 +0200

    More comments added

--- app/test_machine.py
+++ app/test_machine.py
@@ -6,21 +6,33 @@ filename_machine1 = 'app/testing/test-items.csv'
 
 class TestMachine(unittest.TestCase):
 
+    """
+    -   Test scenario where user has enough and not enough coins
+    """
     def test_has_coins(self):
         machine.wallet = [5, 5, 3, 1]
         self.assertTrue(machine.has_coins(4, 4, 2, 1))
         self.assertFalse(machine.has_coins(4, 4, 2, 2))
 
+    """
+    -   Check if adding coins works
+    """
     def test_add_coins(self):
         machine.wallet = [5, 5, 3, 1]
         machine.add_coins(1, 1, 1, 1)
         self.assertTrue(machine.wallet == [6, 6, 4, 2])
 
+    """
+    -   Check if removing coins work properly
+    """
     def test_remove_coins(self):
         machine.wallet = [5, 5, 3, 1]
         machine.remove_coins(1, 1, 1, 1)
         self.assertTrue(machine.wallet == [4, 4, 2, 0])
 
+    """
+    -   Test to see if all the items loaded successfully from a csv file
+    """
     def test_load_items(self):
         items_machine1 = {}
         i = 0
@@ -43,6 +55,9 @@ class TestMachine(unittest.TestCase):
         items_machine1[i] = ['Nik-Naks', 3, 7]
         self.assertEqual(machine.load_items(filename_machine1), items_machine1)
 
+    """
+    -   Test to see if an item is within the given list of items
+    """
     def test_has_item(self):
         machine.load_items(filename_machine1)
         found_item = machine.has_item('Coke')
@@ -52,55 +67,85 @@ class TestMachine(unittest.TestCase):
         self.assertFalse(found_non_item)
         self.assertFalse(zero_item)
 
+    """
+    -   Test to see if the cost of an item is correct 
+    """
     def test_get_item_cost(self):
         machine.load_items(filename_machine1)
         self.assertTrue(machine.get_item_cost('PS') == 10)
 
+    """
+    -   Test to see if the amount of money the user gave is enough
+    """
     def test_is_enough_money(self):
         machine.load_items(filename_machine1)
         self.assertFalse(machine.is_enough_money(9, 'PS'))
         self.assertTrue(machine.is_enough_money(10, 'PS'))
         self.assertTrue(machine.is_enough_money(11, 'PS'))
 
+    """
+    -   Test the helper function to see the quantity of a specific item
+    """
     def test_num_items(self):
         machine.load_items(filename_machine1)
         self.assertTrue(machine.num_items('Fanta'), 5)
 
+    """
+    -   Test to see if an item is successfully deducted from an item
+    """
     def test_deduct_item(self):
         machine.load_items(filename_machine1)
         old_num_cokes = machine.num_items('Coke')
         machine.deduct_item('Coke')
         self.assertTrue(machine.num_items('Coke'), old_num_cokes - 1)
 
+    """
+    -   Test to see if the user change output is enough
+    """
     def test_get_user_change(self):
         machine.load_items(filename_machine1)
         self.assertTrue(20 - machine.get_item_cost('Coke') ==
                         machine.get_user_change(20, machine.get_item_cost('Coke')))
 
+    """
+    -   Test to see if user gets the right amount of R10 notes (coins)
+    """
     def test_get_num_denom_4_change(self):
         machine.load_items(filename_machine1)
         change = machine.denom_4 * 2
         num_denom_4 = machine.get_num_denom_4_change(change)
         self.assertTrue(num_denom_4 == 2)
 
+    """
+    -   Test to see if user gets the right amount of R5 coins
+    """
     def test_get_num_denom_3_change(self):
         machine.load_items(filename_machine1)
         change = machine.denom_4 * 1 + machine.denom_3 * 1
         num_denom_3 = machine.get_num_denom_3_change(change)
         self.assertTrue(num_denom_3 == 1)
 
+    """
+    -   Test to see if user gets the right amount of R2 coins
+    """
     def test_get_num_denom_2_change(self):
         machine.load_items(filename_machine1)
         change = machine.denom_3 * 1 + machine.denom_2 * 1
         num_denom_2 = machine.get_num_denom_2_change(change)
         self.assertTrue(num_denom_2 == 1)
 
+    """
+    -   Test to see if user gets the right amount of R1 coins 
+    """
     def test_get_num_denom_1_change(self):
         machine.load_items(filename_machine1)
         change = machine.denom_2 * 1 + machine.denom_1 * 1
         num_denom_1 = machine.get_num_denom_1_change(change)
         self.assertTrue(num_denom_1 == 1)
 
+    """
+    -   Test helper function that returns the number lines in a file
+    """
     def test_fiile_num_lines(self):
         file = open(filename_machine1, "r")
         num_lines = 10
@@ -108,6 +153,9 @@ class TestMachine(unittest.TestCase):
         file.close()
         self.assertTrue(num_lines == ans)
 
+    """
+    -   Test all possible scenarios of a payment that could be made
+    """
     def test_makePayment(self):
         machine.load_items(filename_machine1)
         machine.wallet = [5, 5, 5, 5]
@@ -123,6 +171,9 @@ class TestMachine(unittest.TestCase):
         self.assertTrue(machine.makePayment(18, 'Lays', 1, 1, 1, 1) ==
                         "You have successfully purchased item = Lays Your change is = R11: 1 x R1, 0 x R2, 0 x R5, 1 x R10 ")
 
+    """
+    -   Test to see if the progress bar closes properly
+    """
     def test_z_close_app(self):
         print("Closing machine progress bar")
         self.assertTrue(machine.close_app())
